2b17b84ec9267830ce948d07d61790131ab6b6ee
Pica4x6 <Pica4x6@users.noreply.github.com>
2021-09-02 11:55:23 +0200

    Add files via upload
    Adding matplotlib querier

--- cobras_ts/querier/__init__.py
+++ cobras_ts/querier/__init__.py
@@ -3,3 +3,4 @@ from .labelquerier import LabelQuerier
 from .querier import Querier
 from .visualquerier import VisualQuerier
 from .visualquerier_images import VisualImageQuerier
+from .matplotquerier import MatplotQuerierTS
